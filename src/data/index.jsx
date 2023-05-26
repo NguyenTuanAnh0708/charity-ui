c0b233907c786d9a0daf18566099c78f4e62ac82
Tai Nguyen <tainguyen880s@gmail.com>
2023-05-26 13:16:24 +0700

    introduce

--- src/data/index.jsx
+++ src/data/index.jsx
@@ -4,5 +4,8 @@ const dataWeb = {
     "https://the350f.com/wp-content/uploads/2021/05/1600x678.jpg",
     "https://the350f.com/wp-content/uploads/2021/06/BO-COVER.jpg",
   ],
+  introduceImg: [
+    "https://the350f.com/wp-content/uploads/2021/05/coconut.jpg"
+  ]
 };
 export default dataWeb;
