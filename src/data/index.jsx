28589be7d7e90fd16c8d20f466ccccebdf0a782a
Nguyen Tuananh <nguyentuananh@Nguyens-MacBook-Air.local>
2023-05-31 12:46:09 +0700

    add ass

--- src/data/index.jsx
+++ src/data/index.jsx
@@ -2,7 +2,7 @@ import momoQr from '../assets/momo.jpg';
 import bankQr from '../assets/bank.jpg';
 import Logo from '../assets/logo.png';
 import slider1 from '../assets/slider1.png';
-import slider2 from '../assets/slider2.png';
+import slider2 from '../assets/slider2.jpg';
 import slider3 from '../assets/slider3.png';
 
 const dataWeb = {
